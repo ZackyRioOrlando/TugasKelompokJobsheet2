90e75bc056082c3c9c0d3a017f312b255b6c86b6
Zacky Rio Orlando <zackyrio555@gmail.com>
2024-09-06 11:21:36 +0700

    Tugas Kelompok Kolaborasi Jobsheet2

---
+++ PerkenalanZackyRioOrlando.java
@@ -0,0 +1,16 @@
+public class PerkenalanZackyRioOrlando{
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Zacky Rio Orlando");
+        System.out.println("NIM     : 244107020086");
+        System.out.println("Kelas   : 1E");
+        System.out.println("No Absen: 27");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : LINGK MACANAN RT/RW 002/008,Petungasri,Pandaan");
+        System.out.println("No HP   : 081216737744");
+        System.out.println("Hobi    : Futsal" );
+
+    }
+}
