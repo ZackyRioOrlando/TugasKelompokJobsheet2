acff55916a49b9a4626e4e56c21f55f8b3e18972
Xyrfo <rofo.anggi18@gmail.com>
2024-09-15 05:38:56 +0700

    Create PerkenalanRifoAnggiBD.java

---
+++ PerkenalanRifoAnggiBD.java
@@ -0,0 +1,16 @@
+public class PerkenalanRifoAnggiBD {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Rifo Anggi Barbara Danuarta");
+        System.out.println("NIM     : 244107020063");
+        System.out.println("Kelas   : 1E");
+        System.out.println("No Absen: 26");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : DSN.SIDORUKUN RT/RW 001/004,Gucialit,Lumajang");
+        System.out.println("No HP   : 085655819596");
+        System.out.println("Hobi    : Lari");
+        
+    }
+}
